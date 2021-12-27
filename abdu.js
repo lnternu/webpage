c973e83b1f9063282db4062e718117dd541e6081
lnternu <94347967+lnternu@users.noreply.github.com>
2021-12-27 14:30:28 +0600

    Add files via upload

---
+++ abdu.js
@@ -0,0 +1,15 @@
+data = {
+    user: "abdu",
+    pass: "2005",
+}
+
+let accept = () =>{
+    let userName = document.getElementById('user').value
+    let password = document.getElementById('pass').value
+    if (data.user == userName && data.pass == password) {
+        window.location.href = '66.228.59.137'
+    }else{
+        alert("Password is error")
+    }
+  
+}
