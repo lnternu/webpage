56a533437bd054f129c2fe1dfeddf366ded40e26
lnternu <94347967+lnternu@users.noreply.github.com>
2021-12-27 14:34:25 +0600

    Update abdu.js

--- abdu.js
+++ abdu.js
@@ -7,7 +7,7 @@ let accept = () =>{
     let userName = document.getElementById('user').value
     let password = document.getElementById('pass').value
     if (data.user == userName && data.pass == password) {
-        window.location.href = '66.228.59.137'
+        window.location.href = 'http://66.228.59.137'
     }else{
         alert("Password is error")
     }
